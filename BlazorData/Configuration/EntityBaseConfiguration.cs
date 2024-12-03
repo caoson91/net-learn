e8a6795c57a4356728982ce33bb8df9e38c2377e
caoson91 <nguyencaoson91@gmail.com>
2024-12-03 22:36:18 +0700

    add unit of work

---
+++ BlazorData/Configuration/EntityBaseConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorData.Configuration
+{
+    internal class EntityBaseConfiguration<T> : IEntityTypeConfiguration<T> where T : EntityBase
+    {
+        protected virtual bool ApplyQueryFilter => true;
+        public virtual void Configure(EntityTypeBuilder<T> builder)
+        {
+            builder.ToTable(typeof(T).Name);
+
+            builder.Property(e => e.Id)
+                .HasDefaultValueSql("newsequentialid()");
+
+            builder.Property(e => e.IsDeleted)
+                .HasDefaultValue(false);
+
+            if (ApplyQueryFilter)
+            {
+                builder.HasQueryFilter(p => p.IsDeleted == false);
+            }
+
+            builder.HasOne(e => e.Creator)
+                .WithMany()
+                .HasForeignKey(e => e.CreatedBy)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            builder.HasOne(e => e.UpdatedByUser)
+                .WithMany()
+                .HasForeignKey(e => e.UpdatedBy)
+                .OnDelete(DeleteBehavior.NoAction);
+        }
+    }
+}
---
+++ BlazorRepository/UnitOfWork.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorRepository
+{
+    public class UnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+        public IUsersRepository Users { get; }
+        public UnitOfWork(ApplicationDbContext context, IUsersRepository usersRepository)
+        {
+            _context = context;
+            Users = usersRepository;
+        }
+
+        public async Task<int> CompleteAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _context.Dispose();
+            }
+        }
+    }
+}
